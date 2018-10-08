5368aecbfd53f0963cd781c8b5aa28956c524125
Parul Chandel <parul.chandel14@gmail.com>
2018-10-08 15:49:37 +0530

    Added Socket Server File

---
+++ Basics/socket_server.py
@@ -0,0 +1,23 @@
+import sys, socket, threading
+from queue import Queue
+
+
+host = "127.0.0.1"
+port = 3000
+buffer_size = "1024"
+try:
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+except socket.error as e:
+    print(e)
+tcp_socket.bind((host, port))
+tcp_socket.listen(2)
+print("Listening .... \n")
+while True:
+    connection, address = tcp_socket.accept()
+    print("Connection Established !! \n")
+    print(address)
+    data = connection.recv(1024)
+    print("Data : ", data)
+    connection.close()
+tcp_socket.close()
